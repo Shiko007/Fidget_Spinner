5698080790895a5f497cca5a7b654572b213c0bc
Shiko <sherifyasser92@gmail.com>
2021-03-21 15:09:39 +0100

    Added background color changing functionality

--- FidgetSpinner/FidgetSpinner/Modules/TouchableNode.swift
+++ FidgetSpinner/FidgetSpinner/Modules/TouchableNode.swift
@@ -4,8 +4,8 @@
 //
 //  Created by Sherif Yasser on 06.03.21.
 //
-
 import SpriteKit
+import CoreGraphics
 
 class TouchableSpriteNode : SKSpriteNode
 {
@@ -35,6 +35,25 @@ class TouchableSpriteNode : SKSpriteNode
             let backgroundNode = self.scene?.childNode(withName: Configurations().backgroundImagePrefix) as! SKShapeNode
             let backgroundImageName = self.name?.replacingOccurrences(of: Configurations().backgroundImageSampleSuffix, with: "")
             backgroundNode.fillTexture = SKTexture(imageNamed: backgroundImageName!)
+        }
+        else if((self.name?.contains(Configurations().backgroundImagePrefix))! && ((self.name?.contains(Configurations().backgroundColorSampleSuffix))!)){
+            let backgroundNode = self.scene?.childNode(withName: Configurations().backgroundImagePrefix) as! SKShapeNode
+            let prefix = Configurations().backgroundImagePrefix
+            let suffix = Configurations().backgroundColorSampleSuffix
+            if let color = (UIImage(named: self.name!)!)[100,100]{
+                //Workaround the black not being detected as black issue
+                if(self.name! == prefix + "0" + suffix){
+                    backgroundNode.fillColor = .black
+                }
+                else{
+                    var red: CGFloat = 0.0
+                    var green: CGFloat = 0.0
+                    var blue: CGFloat = 0.0
+                    var alpha: CGFloat = 0.0
+                    color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+                    backgroundNode.fillColor = color
+                }
+            }
         }
     }
 }
